5b7a48984a0a980de8a394c70dbb7c4378979170
yudlinh0810 <yudlinh0810@gmail.com>
2025-02-20 02:06:49 +0700

    feat: add Icon Destination componenet use scss

---
+++ src/components/IconDestination.tsx
@@ -0,0 +1,7 @@
+import styled from "../styles/modules/components/IconDestination.module.scss";
+
+const IconDestination = () => {
+  return <div className={styled["ic-destination"]}></div>;
+};
+
+export default IconDestination;
