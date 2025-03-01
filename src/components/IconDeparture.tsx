f9f244c1994164989cf30dde3fe3a1819bbebc56
yudlinh0810 <yudlinh0810@gmail.com>
2025-03-01 18:47:19 +0700

    feat: add IconDeparture component with scss styles

---
+++ src/components/IconDeparture.tsx
@@ -0,0 +1,7 @@
+import styled from "../styles/modules/components/IconDeparture.module.scss";
+
+const IconDeparture = () => {
+  return <div className={styled["ic-departure"]}></div>;
+};
+
+export default IconDeparture;
